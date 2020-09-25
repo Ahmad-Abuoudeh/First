4df8cf4dc72b89720bfa2573a4ffce3875fc725a
Ahmad <lion1000asd@yahoo.com>
2020-09-25 09:11:59 -0400

    I add some functions calls to AddressBook.java to test GIT commit

--- AddressBook.java
+++ AddressBook.java
@@ -2,19 +2,18 @@ import java.util.ArrayList;
 
 public class AddressBook {
 
-    ArrayList<Object> str = new ArrayList<>() ;
-    public void AddBuddy( String name , String address){
+    ArrayList<Object> str = new ArrayList<>();
+
+    public void AddBuddy(String name, String address) {
         BuddyInfo bud = new BuddyInfo();
-        BuddyInfo.name = name  ;
-        bud.address = address ;
+        BuddyInfo.name = name;
+        bud.address = address;
         str.add(bud);
     }
 
-    public void RemoveBuddy( String name) {
-        str.remove( name) ;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Address Book");
+    public void RemoveBuddy(String name) {
+        str.remove(name);
     }
 }
+
+
